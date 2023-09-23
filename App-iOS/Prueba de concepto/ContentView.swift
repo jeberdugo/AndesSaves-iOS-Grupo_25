e8196ca14ed3c69c55e4710e03e860cfbafeb0f6
Mateo <m.martinezp@uniandes.edu.co>
2023-09-23 14:02:41 -0500

    Budget, Transaction and Settings View implementation

--- App-iOS/Prueba de concepto/ContentView.swift
+++ App-iOS/Prueba de concepto/ContentView.swift
@@ -16,9 +16,20 @@ struct FinanceApp: App {
     }
 }
 
+
+
 struct ContentView: View {
     @State private var balance: Double = 1000.0 // Initial balance
     @State private var isAddingTransaction = false
+    @State private var transactionName = ""
+    @State private var transactionAmount = ""
+    @State private var transactionSource = ""
+    @State private var selectedType: Int = 0 // 0 for Income, 1 for Expense
+    @State private var selectedExpenseCategory: Int = 0
+    
+    var expenseCategories = ["Food", "Transport", "House", "Others"]
+    
+    
     
     var body: some View {
         NavigationView {
@@ -48,7 +59,62 @@ struct ContentView: View {
                                 .cornerRadius(10)
                         }
                         .sheet(isPresented: $isAddingTransaction) {
+                            ZStack() {
+                                //Color(red: 21/255, green: 191/255, blue: 129/255).edgesIgnoringSafeArea(.all)
+                                Color(selectedType == 1 ? .red : .green) // Red for Expense, Green for Income
+                                                        .edgesIgnoringSafeArea(.all)
+                                VStack {
+                                    Text("History")
+                                        .font(.title)
+                                        .fontWeight(.bold)
+                                        .foregroundColor(.white)
+                                }
+                            }
+                            .frame(maxWidth: 400, maxHeight: 60)
+                            Spacer()
                             // Add transaction view goes here
+                            Form {
+                                            Section(header: Text("Transaction Details")) {
+                                                TextField("Name", text: $transactionName)
+                                                TextField("Amount", text: $transactionAmount)
+                                                TextField("Source", text: $transactionSource)
+                                            }
+
+                                            Section(header: Text("Type")) {
+                                                Picker(selection: $selectedType, label: Text("Type")) {
+                                                    Text("Income").tag(0)
+                                                    Text("Expense").tag(1)
+                                                }
+                                                .pickerStyle(SegmentedPickerStyle())
+                                                .padding(.horizontal)
+                                            }
+
+                                            if selectedType == 1 {
+                                                Section(header: Text("Expense Category")) {
+                                                    Picker("Select Category", selection: $selectedExpenseCategory) {
+                                                        ForEach(0..<expenseCategories.count) { index in
+                                                            Text(expenseCategories[index])
+                                                        }
+                                                    }
+                                                    .pickerStyle(SegmentedPickerStyle())
+                                                }
+                                            }
+                                        }
+
+                                        Button(action: {
+                                            // Add action logic here to save the transaction
+                                        }) {
+                                            Text("Add")
+                                                .foregroundColor(.white)
+                                                .frame(maxWidth: .infinity)
+                                                .padding()
+                                                .background(selectedType == 1 ? Color.red : Color.green)
+                                                .cornerRadius(10)
+                                        }
+                                        .padding()
+                            
+                            
+                            
                         }
                     }
                     Spacer(minLength: 10)
@@ -213,20 +279,224 @@ struct Transaction: Identifiable {
     
 // Vista para "Budgets"
     struct BudgetsView: View {
+        @State private var isAddBudgetViewPresented = false
+        
+            var body: some View {
+                
+                    ZStack() {
+                        Color(red: 21/255, green: 191/255, blue: 129/255).edgesIgnoringSafeArea(.all)
+                        VStack {
+                            Text("Budgets")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                        }
+                    }.frame(maxWidth: 400, maxHeight: 60)
+                    Spacer()
+                    VStack {
+                        HStack(spacing: 30) {
+                            
+                            NavigationLink(destination: AddBudgetView(), isActive: $isAddBudgetViewPresented) {
+                                EmptyView()
+                            }
+                            
+                            Button(action: {
+                                isAddBudgetViewPresented.toggle()
+                            }) {
+                            VStack {
+                                
+                                    Image(systemName: "plus.rectangle.fill")
+                                        .resizable()
+                                        .frame(width: 40, height: 40) // Increase size
+                                        .foregroundColor(.green) // Set color to green
+                                        .background(Color.white.opacity(0.2)) // Background color
+                                        .cornerRadius(5)
+                                    Text("Add")
+                                }}
+                            
+                            
+                            VStack {
+                                Image(systemName: "minus.rectangle.fill")
+                                    .resizable()
+                                    .frame(width: 40, height: 40) // Increase size
+                                    .foregroundColor(.red) // Set color to red
+                                    .background(Color.white.opacity(0.2)) // Background color
+                                    .cornerRadius(5)
+                                Text("Remove")
+                            }
+                            .onTapGesture {
+                                // Remove action logic here
+                            }
+                        }
+                        .padding(.horizontal)
+                        
+                        // Section: Individual
+                        Text("Individual")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .padding(.top, 20)
+                        
+                        ItemRow(name: "House", date: "2023-09-25", percentage: "60%")
+                        
+                        Divider()
+                        
+                        ItemRow(name: "Car", date: "2023-09-26", percentage: "80%")
+                        
+                        // Section: Group
+                        Text("Group")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .padding(.top, 20)
+                        
+                        ItemRow(name: "Europe", date: "2023-09-27", percentage: "75%")
+                        
+                        Divider()
+                        
+                        ItemRow(name: "Car", date: "2023-09-28", percentage: "90%")
+                    }
+                    .padding(.top, -650)
+                    .background(Color.white)
+                    
+                
+            }
+        
+    }
+    
+    
+    
+    struct ItemRow: View {
+        var name: String
+        var date: String
+        var percentage: String
+        
+        var body: some View {
+            HStack {
+                VStack{
+                    Text(name)
+                        .font(.headline)
+                        .frame(width: 100)
+                    
+                    Text(date)
+                        .font(.subheadline)
+                }
+                
+                
+                Spacer()
+                
+                Text(percentage)
+                    .font(.subheadline)
+            }
+            .padding(.horizontal)
+            .padding(.vertical, 5)
+        }
+    }
+
+    struct BudgetsView_Previews: PreviewProvider {
+        static var previews: some View {
+            BudgetsView()
+        }
+    }
+    
+    struct AddBudgetView: View {
+        @State private var budgetName = ""
+        @State private var budgetAmount = ""
+        @State private var budgetDate = Date()
+        @State private var selectedType = 0 // 0 for Individual, 1 for Group
+        @State private var memberName = ""
+        @State private var groupMembers: [String] = []
         var body: some View {
             ZStack() {
                 Color(red: 21/255, green: 191/255, blue: 129/255).edgesIgnoringSafeArea(.all)
                 VStack {
-                    Text("Budgets")
+                    Text("Add Budgets")
                         .font(.title)
                         .fontWeight(.bold)
                         .foregroundColor(.white)
                 }
             }.frame(maxWidth: 400, maxHeight: 60)
             Spacer()
-            VStack() {
+            VStack {
+                // Budget Entry Form
+                Text("Name")
+                    .font(.headline)
+                    .padding(.top, 20)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .offset(x: 20)
                 
-            }.background(Color.white)
+                TextField("Enter name", text: $budgetName)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal)
+                
+                Text("Amount")
+                    .font(.headline)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .offset(x: 20)
+                
+                TextField("Enter amount", text: $budgetAmount)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal)
+                
+                Text("Date")
+                    .font(.headline)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .offset(x: 20)
+                
+                DatePicker("", selection: $budgetDate, displayedComponents: .date)
+                    .datePickerStyle(DefaultDatePickerStyle())
+                    .padding(.horizontal)
+                    
+                    
+                
+                Text("Type")
+                    .font(.headline)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .offset(x: 20)
+                
+                Picker(selection: $selectedType, label: Text("Type")) {
+                    Text("Individual").tag(0)
+                    Text("Group").tag(1)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding(.horizontal)
+                
+                if selectedType == 1 {
+                    Text("Members")
+                        .font(.headline)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .offset(x: 20)
+                    
+                    TextField("Enter member name", text: $memberName)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal)
+                    
+                    Button(action: {
+                        groupMembers.append(memberName)
+                        memberName = ""
+                    }) {
+                        Text("Add Member")
+                            .foregroundColor(.green)
+                    }
+                }
+                
+                Button(action: {
+                    // Add budget entry logic here
+                }) {
+                    Text("Add")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.green)
+                        .cornerRadius(10)
+                }.padding()
+                
+            
+                
+            }.padding(.top, -650)
+    }
+    }
+
+    struct AddBudgetView_Previews: PreviewProvider {
+        static var previews: some View {
+            AddBudgetView()
         }
     }
 
@@ -295,6 +565,7 @@ struct Transaction: Identifiable {
     
 
 // Vista para "Settings"
+    
     struct SettingsView: View {
         var body: some View {
             ZStack() {
@@ -307,9 +578,69 @@ struct Transaction: Identifiable {
                 }
             }.frame(maxWidth: 400, maxHeight: 60)
             Spacer()
+            
             VStack() {
+                    HStack() {
+                        Image(systemName: "dollarsign.circle")
+                            .foregroundColor(.blue)
+                            .padding(.leading, 16)
+                            .frame(width: 30, height: 30)
+                        
+                        SectionView(title: "Currency")
+                            .font(.headline)
+                            .padding(.leading, 16)
+                            .foregroundColor(.primary)
+                        
+                        Spacer()}
+                    
+                    Divider()
+                    
+                    HStack(){
+                        Image(systemName: "globe")
+                            .foregroundColor(.blue)
+                            .padding(.leading, 16)
+                            .frame(width: 30, height: 30)
+                        
+                        SectionView(title: "Language")
+                            .font(.headline)
+                            .padding(.leading, 16)
+                            .foregroundColor(.primary)
+                        
+                        Spacer()}
+                    
+                    Divider()
+                    
+                    HStack() {
+                        Image(systemName: "bell")
+                            .foregroundColor(.blue)
+                            .padding(.leading, 16)
+                            .frame(width: 30, height: 30)
+                        
+                        SectionView(title: "Notifications")
+                            .font(.headline)
+                            .padding(.leading, 16)
+                            .foregroundColor(.primary)
+                        
+                        Spacer()}
+                    
+                }.background(Color.white)
+                .padding(.top, -650)
+            
+        }
+    }
+    
+    struct SectionView: View {
+        var title: String
+        
+        var body: some View {
+            HStack {
+                Text(title)
+                    .font(.headline)
+                    .padding(.leading, 16)
                 
-            }.background(Color.white)
+                Spacer()
+            }
+            .frame(height: 40) // Adjust the height as needed
         }
     }
 
